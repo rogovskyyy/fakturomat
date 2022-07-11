0cf89e8768e205f1332b1001ce6efe8e03369444
rogovskyyy <barrog@st.amu.edu.pl>
2022-07-11 21:48:39 +0200

    Fixes

--- resources/views/invoice/add.blade.php
+++ resources/views/invoice/add.blade.php
@@ -29,7 +29,7 @@
                         <input type="date" name="name" class="form-control" placeholder="Nazwa kontrahenta" aria-label="First name">
                     </div>
                 </div> <br />
-            <table class="table">
+            <table class="table" id="table">
                 <tr>
                     <th>Nazwa</th>
                     <th>Miara</th>
@@ -48,20 +48,20 @@
                             <option>rg.</option>
                         </select>
                     </td>
-                    <td><input type="text" name="name" class="form-control" placeholder="Ilość" aria-label="Last name"></td>
-                    <td><input type="text" name="name" class="form-control" placeholder="Cena netto" aria-label="Last name"></td>
-                    <td>123,00 zł</td>
+                    <td><input type="text" id='ilosc' name="name" class="form-control" placeholder="Ilość" aria-label="Last name"></td>
+                    <td><input type="text" id='cena_netto' name="name" class="form-control" placeholder="Cena netto" aria-label="Last name"></td>
+                    <td><div id="wartosc_netto">0,00</div></td>
                     <td>
-                        <select class="form-control">
-                            <option>zw.</option>
-                            <option>0 %</option>
-                            <option>5 %</option>
-                            <option>8 %</option>
-                            <option>23 %</option>
+                        <select class="form-control" id="vat">
+                            <option value="0">zw.</option>
+                            <option value="0">0 %</option>
+                            <option value="5">5 %</option>
+                            <option value="8">8 %</option>
+                            <option value="23">23 %</option>
                         </select>
                     </td>
-                    <td>123,00 zł</td>
-                    <td>123,00 zł</td>
+                    <td><div id="kwota_vat">0,00 zł</div></td>
+                    <td><div id="wartosc_brutto">0,00 zł</div></td>
                 </tr>
             </table>
             <button type="button" class="btn btn-danger btn-sm float-right">Dodaj</button>
@@ -72,6 +72,45 @@
     </div>
 </div>
 <script>
+
+    function refresh_table() {
+        let x = 0;
+        $('#table tr').each(function() {
+            if(x == 0) { }
+            else {
+                // Pełna lista TD
+                let data = $(this).find('td').each (function() { });
+
+                let ilosc = $('#ilosc').val();
+                let cena_netto = $('#cena_netto').val();
+                let wartosc_netto = parseFloat(ilosc * cena_netto).toFixed(2);
+                $('#wartosc_netto').text(wartosc_netto + " zł");
+
+                let vat = $('#vat').val();
+
+                let kwota_vat = parseFloat((wartosc_netto / 100) * vat).toFixed(2);
+
+                $('#kwota_vat').text(kwota_vat + " zł")
+
+                let total = parseFloat(wartosc_netto) + parseFloat(kwota_vat);
+                let wartosc_brutto = parseFloat(total).toFixed(2);
+                $('#wartosc_brutto').text(wartosc_brutto + " zł");
+            }
+            x++;
+        });
+    }
+
+    $( document ).ready(function() {
+        refresh_table();
+    });
+
+    $('#table').on('change', 'input', function() {
+        refresh_table();
+    });
+
+    $('#vat').on('change', function() {
+        refresh_table();
+    })
 
 </script>
 
